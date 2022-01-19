c85e7c1a5b1d46b2e06dfd54aff629c322ad4c74
LutsenkoAnton21 <91205463+LutsenkoAnton21@users.noreply.github.com>
2022-01-19 17:35:41 +0300

    Add files via upload

---
+++ DogsHouseService/Entities/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogsHouseService.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public decimal Length { get; set; }
+        public decimal Weight { get; set; }
+
+
+        public(object, Type) GetAttributeWithType(string name)
+        {
+            var field = typeof(Dog).GetProperty(name);
+            if (field == null) throw new Exception();
+            var value = field.GetValue(this);
+            return (value, field.PropertyType);
+        }
+    }
+}
---
+++ DogsHouseService/Models/DogModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogsHouseService.Models
+{
+    public class DogModel
+    {
+        [Required(ErrorMessage ="Need to name dog")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Need to color dog")]
+        public string Color { get; set; }
+        [Required]
+        [Range(0, 2000, ErrorMessage = "TailLength can't be negative")]
+        public decimal Length { get; set; }
+        [Required]
+        [Range(0, 2000, ErrorMessage = "Weight can't be negative")]
+        public decimal Weight { get; set; }
+
+    }
+}
